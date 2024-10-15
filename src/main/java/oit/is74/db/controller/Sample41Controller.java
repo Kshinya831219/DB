102acc9049b63357052060d2fa895bb6f1f201f8
Kshinya831219 <e1b22036@oit.ac.jp>
2024-10-15 16:12:17 +0900

    複数のテーブルに登録されたデータをJOINして表示

--- src/main/java/oit/is74/db/controller/Sample41Controller.java
+++ src/main/java/oit/is74/db/controller/Sample41Controller.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import oit.is74.db.model.Chamber;
 import oit.is74.db.model.ChamberMapper;
-//import oit.is74.db.model.ChamberUser;
+import oit.is74.db.model.ChamberUser;
 //import oit.is74.db.model.UserInfo;
 
 @Controller
@@ -72,4 +72,13 @@ public class Sample41Controller {
     model.addAttribute("chambers5", chambers5);
     return "sample45.html";
   }
+
+  @GetMapping("step7")
+  @Transactional
+  public String sample47(ModelMap model) {
+    ArrayList<ChamberUser> chamberUsers7 = chamberMapper.selectAllChamberUser();
+    model.addAttribute("chamberUsers7", chamberUsers7);
+    return "sample46.html";
+  }
+
 }
